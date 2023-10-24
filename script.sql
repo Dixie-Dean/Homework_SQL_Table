5acf7e818e6b75024c2fe41dac676a0fbdbbf84e
Dixie Dean <d.w.sokolov26@gmail.com>
2023-10-24 13:50:29 +0300

    Initial Commit

---
+++ script.sql
@@ -0,0 +1,19 @@
+create schema persons
+
+create table persons.PERSONS(
+    name varchar,
+    surname varchar,
+    age integer,
+    phone_number integer,
+    city_of_living varchar,
+    primary key (name, surname, age)
+);
+
+insert into persons.PERSONS(name, surname, age, phone_number, city_of_living)
+VALUES ('Bob', 'Dylan', 82, 123, 'Duluth');
+insert into persons.PERSONS(name, surname, age, phone_number, city_of_living)
+VALUES ('Tom', 'Cruise', 61, 456, 'Syracuse');
+insert into persons.PERSONS(name, surname, age, phone_number, city_of_living)
+VALUES ('Grettir', 'Asmundson', 25, 789, 'Egilsstaðir')
+
+
